43f37f7be0d470de6de1adf5d4d6066bc55d6315
Wellington Dyon Fuck Rutes <wellington.rutes@rga.com>
2022-07-15 16:23:11 +0000

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sum = exports.getRepository = exports.getChannel = void 0;
+var getChannel = function () { return 'https://www.youtube.com/'; };
+exports.getChannel = getChannel;
+var getRepository = function () { return 'https://github.com/'; };
+exports.getRepository = getRepository;
+var sum = function (a, b) { return a + b; };
+exports.sum = sum;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const getChannel = () => 'https://www.youtube.com/'
+
+export const getRepository = () => 'https://github.com/'
+
+export const sum = (a: number, b: number) => a + b;
